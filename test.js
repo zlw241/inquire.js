772e448b189fa58cfd95f5ef7e1941ba50e608e3
Zach Ward <zach.l.ward6@gmail.com>
2017-05-02 15:13:57 -0400

    add source files

---
+++ test.js
@@ -0,0 +1,15 @@
+
+inquire(() => {
+  alert("the document is ready")
+
+  // let root = "https://jsonplaceholder.typicode.com"
+  // inquire.ajax({
+  //   url: root + '/posts',
+  //   method: 'GET',
+  //   success: (data) => {
+  //     console.log(data)
+  //   }
+  // });
+
+  
+});
